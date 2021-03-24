58cf146a78bddba35205920d19e0eb56a2e1a81b
Derek Murphy <derekmurphy1993@gmail.com>
2021-03-24 15:00:15 -0400

    Built out Resources Props
    Resources will return props, no connected to endpoints.
    Resources will render or not based on visibility prop

--- src/App.js
+++ src/App.js
@@ -7,12 +7,23 @@ import Resources from './components/Resources/Resources'
 
 
 class App extends Component {
+  state = {
+    result_visibility: false
+  }
+
+  // this.setState({result_visibility: true})
+
 render() {
   return (
     <div>
     <Layout>
     <Form />
-    <Resources />
+    <Resources
+      LoanAmount='60-70,000'
+      Lender='TestBank'
+      LoanType='CARES PPP'
+      Accuracy="80%"
+      Visible={this.state.result_visibility}/>
     </Layout>
     </div>
   );
--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -4,7 +4,16 @@ import style from './Resources.module.css'
 const resources = (props) => {
 
   return (
-    <h2 style={style.test}> This will be the results! </h2>
+    <div className={style.wrapper}>
+    <p> You may be eligible for </p>
+    <h2 className={style.test}> ${props.LoanAmount}* </h2>
+    <p> {props.Lender} - {props.LoanType} </p>
+
+    <p> Based on the information provided, we estimate you are eligible for PROPS.MONEY
+    from {props.Lender} as a {props.LoanType}. While we can not gaurentee, this would be a
+    good place to start. Try selecting a different lender for another result.</p>
+    <p> Results are predicted with {props.Accuracy} accuracy. No amounts are gaurenteed. </p>
+    </div>
     )
   }
 
