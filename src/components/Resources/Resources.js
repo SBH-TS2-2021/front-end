9f3508248982b2d94e1b191764c8a30c8b5289bf
Derek Murphy <derekmurphy1993@gmail.com>
2021-03-25 12:26:53 -0400

    Removed Unused Component, PreReact Reveal

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import style from './Resources.module.css'
+import Tada from 'react-reveal/Tada';
 
 import LoanRange from './LoanRange/LoanRange'
 
@@ -8,9 +9,11 @@ const resources = (props) => {
   let mostAccurate = props.LoanAmount.indexOf(Math.max(...props.LoanAmount))
 
   return (
-    <div className={style.wrapper}>
+    <div id="loan-result" className={style.wrapper}>
     <p> You may be eligible for</p>
+    <Tada>
     <LoanRange accurateRange={mostAccurate} />
+    </Tada>
     <p> {props.Lender} - {props.LoanType} </p>
 
     <p> from {props.Lender}.</p>
