0f9d4ab143b474688d20ba8fe18a0e2c5a0ede3c
Derek Murphy <derekmurphy1993@gmail.com>
2021-03-25 10:04:02 -0400

    Updated Loan Ranges and Error Case

--- src/components/Resources/LoanRange/LoanRange.js
+++ src/components/Resources/LoanRange/LoanRange.js
@@ -9,25 +9,22 @@ class LoanRange extends Component {
 
     switch ( this.props.accurateRange ) {
       case (0):
-          loan = <h2 className={classes.xxl}>A billion :)</h2>
+          loan = <h1 className={classes.loan5to10mil}>"$5 - 10 million*"</h1>
           break;
       case (1):
-          loan = <h2 className={classes.xl}>Million!</h2>
+          loan = <h1 className={classes.loan2to5mil}>"$2 - 5 million*"</h1>
           break;
       case (2):
-          loan = <h2 className={classes.l}>6 figures!</h2>
+          loan = <h1 className={classes.loan350kto1mil}>"$350,000 - 1 million*"</h1>
           break;
       case (3):
-          loan = <h2 className={classes.m}>Twenty Thousand</h2>
+          loan = <h1 className={classes.loan150to350k}>"$150,000 - 350,000*"</h1>
           break;
       case (4):
-          loan = <h2 className={classes.s}>Hundo's</h2>
-          break;
-      case (5):
-          loan = <h2 className={classes.xs}>Three Dollars</h2>
+          loan = <h1 className={classes.loan1to150k}>  "$1,000 - 150,000*"</h1>
           break;
       default:
-          loan = null
+          loan = <h1 className={classes.noData}>  "Error - No Data Avalible"</h1>
         }
         return loan
     }
--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -13,9 +13,7 @@ const resources = (props) => {
     <LoanRange accurateRange={mostAccurate} />
     <p> {props.Lender} - {props.LoanType} </p>
 
-    <p> Based on the information provided, we estimate you are eligible for PROPS.MONEY
-    from {props.Lender} as a {props.LoanType}. While we can not gaurentee, this would be a
-    good place to start. Try selecting a different lender for another result.</p>
+    <p> from {props.Lender}.</p>
     <p> *Results were predicted with {Math.round(props.LoanAmount[mostAccurate]*100)}% accuracy. No amounts are gaurenteed. </p>
     </div>
     )
